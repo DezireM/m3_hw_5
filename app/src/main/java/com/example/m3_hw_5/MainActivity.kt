345995e0336573616ed50619d4be7c98d8659c56
DezireM <deziremaatkabylova@gmail.com>
2024-06-05 17:40:29 +0600

    Initial commit

---
+++ app/src/main/java/com/example/m3_hw_5/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.m3_hw_5
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction()
+            .add(R.id.container, CountFragment())
+            .commit()
+
+    }
+}
